ca2ac584a1b76026aa7107612f4bdd534f719acb
Thangnt <thangnt336@gmail.com>
2021-07-17 02:20:17 -0400

    added homepage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Contact() {
-    return (
-        <section id="contactForm">
-        <div>Hello</div>
-      </section>
-    );
+   <form method="POST">
+   <label htmlFor="name">Name</label>
+   <input type="text" name="name" />
+
+   <label htmlFor="email">Email</label>
+   <input type="email" name="email" />
+
+   <label htmlFor="message">Message</label>
+   <textarea name="message" rows="3"></textarea>
+
+   <input type="submit" />
+ </form>
 }
 
 export default Contact;
