f0d8db21894e5f845eca0e329e7ce612c5275e77
Thangnt <thangnt336@gmail.com>
2021-12-17 14:41:47 -0500

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ function About() {
         <h2>About</h2>
     <div className="aboutMe">
         <img src={headshot} alt="My Head"/>
-        <p>Welcome to my page! My name is Thang. I am a recent graduate from UNC's Coding Bootcamp. I am a Full Stack Web
-          Developer with proficiencies in JavaScript, CSS, React, Node.js, and MySQL. <br></br> When I'm not inside coding, I enjoy going hiking or to the park with my dog. My
-          hobbies include playing guitar, cooking,running, and driving (drifting).</p>
+        <p>Welcome to my page! My name is Chris and I am a recent graduate from UNC's Coding Bootcamp. I am currently a Full Stack Web
+          Developer with a focus on Front End Development using React.<br></br> When I'm not inside coding, I enjoy going hiking or to the park with my dog. My
+          hobbies include playing guitar, cooking,running, and drifting.</p>
           </div>
     </section>
     );
