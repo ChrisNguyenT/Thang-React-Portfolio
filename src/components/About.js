ff949219e5111d51fac1402d9d7702fe75fce2a7
Thangnt <thangnt336@gmail.com>
2022-01-12 21:44:07 -0500

    editted about

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,7 @@ function About() {
         <h2>About</h2>
     <div className="aboutMe">
         <img src={headshot} alt="My Head"/>
-        <p>Welcome to my page! My name is Chris and I am a recent graduate from UNC's Coding Bootcamp. I am currently a Full Stack Web
-          Developer with a focus on Front End Development using React.<br></br> When I'm not inside coding, I enjoy going hiking or to the park with my dog. My
-          hobbies include playing guitar, cooking, running, and drifting.</p>
+        <p>Welcome to my page! My name is Chris and I'm currently a freelance web developer. If you need any websites designed and built, I'm your guy.</p>
           </div>
     </section>
     );
